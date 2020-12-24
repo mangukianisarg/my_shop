a17a7589791ed92d49e876ce23160c230e398149
Nisarg Mangukiya <mangukianisarg@gmail.com>
2020-12-24 11:09:35 +0530

    Starting Build App

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:my_shop/providers/products.dart';
 import 'package:my_shop/screen/product_detail_screen.dart';
 import 'package:my_shop/screen/product_overview_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.amber,
-        accentColor: Colors.white,
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'MyShop',
+        theme: ThemeData(
+          primarySwatch: Colors.amber,
+          accentColor: Colors.white,
+          fontFamily: 'Lato',
+        ),
+        home: ProductOverViewScreen(),
+        routes: {
+          ProductDetailsScreen.routeName: (ctx) => ProductDetailsScreen()
+        },
       ),
-      home: ProductOverViewScreen(),
-      routes: {ProductDetailsScreen.routeName: (ctx) => ProductDetailsScreen()},
     );
   }
 }
