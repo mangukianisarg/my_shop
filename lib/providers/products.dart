3cc951c165fe457f8d66d13257a8104104ab6255
Nisarg Mangukiya <mangukianisarg@gmail.com>
2020-12-24 14:08:28 +0530

    Starting Build App

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -11,6 +11,7 @@ class Products with ChangeNotifier {
       price: 29.99,
       imageUrl:
           'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+      // isFavorite: false,
     ),
     Product(
       id: 'p2',
@@ -19,6 +20,7 @@ class Products with ChangeNotifier {
       price: 59.99,
       imageUrl:
           'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
+      // isFavorite: false,
     ),
     Product(
       id: 'p3',
@@ -27,6 +29,7 @@ class Products with ChangeNotifier {
       price: 19.99,
       imageUrl:
           'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
+      // isFavorite: false,
     ),
     Product(
       id: 'p4',
@@ -35,6 +38,7 @@ class Products with ChangeNotifier {
       price: 49.99,
       imageUrl:
           'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
+      // isFavorite: false,
     ),
   ];
 
